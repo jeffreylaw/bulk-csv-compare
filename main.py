8294e13597feb3bd28d93572d7abdcd097f2f2be
Jeffrey Law <jylday56@gmail.com>
2021-02-04 12:42:02 -0800

    Changing to use contents inside a before and after folder

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import json
 
 def compare_files(file1, file2):
     filename = os.path.basename(file1).split('-')[0]
-    with open(file1, 'r', encoding="utf8") as file1, open(file2, 'r', encoding="utf8") as file2:
+    with open(file1, 'r', encoding='utf8') as file1, open(file2, 'r', encoding='utf8') as file2:
         reader1 = csv.reader(file1)
         reader2 = csv.reader(file2)
 
@@ -28,32 +28,33 @@ def compare_files(file1, file2):
             file2_chat[message_num] = ''.join(row)
             message_num += 1
 
-        result_dict = {filename: "Pass"}
+        result_dict = {filename: 'Pass'}
 
         for i in file1_chat:
             try:
                 if (file1_chat[i] != file2_chat[i]):
                     print('\n')
-                    print(filename.center(80, '='))
+                    print(filename)
                     print('EXPECTED MESSAGE'.center(80, '-'))
                     print(file1_chat[i])
                     print('FOUND MESSAGE'.center(80, '-'))
                     print(file2_chat[i])
                     print('-' * 80)
                     print(
-                        f"\nError: Chat histories don't match. See line {i+7} in the Excel file.")
-                    result_dict = {filename: "Failed"}
+                        f"\nError: Chat histories don't match. See row {i+7} in the Excel file.")
+                    result_dict = {filename: 'Fail'}
                     break
             except Exception:
-                # print()
-                # print()
-                # print('Filename: ' + filename)
-                # print(str(i) + ': ' + file1_chat[i] + ' (expected)')
-                # print('~')
-                # print('Error, line not found')
-                # print(
-                #     '|---------------------------------------------------------------------------------------------|\n')
-                result_dict = {filename: "Failed"}
+                print('\n')
+                print(filename)
+                print('EXPECTED MESSAGE'.center(80, '-'))
+                print(file1_chat[i])
+                print('-' * 80)
+                print('Error: Missing messages')
+                print(
+                    f"\nError: Chat histories don't match. See row {i+7} in the Excel file.")
+
+                result_dict = {filename: 'Fail'}
     return result_dict
 
 
@@ -67,22 +68,42 @@ if __name__ == '__main__':
         results.update(result)
 
     if len(sys.argv) == 1:
+        if not os.path.exists('before') or not os.path.isdir('before'):
+            print('Missing \'before\' directory')
+            exit(1)
+        if not os.path.exists('after') or not os.path.isdir('after'):
+            print('Missing \'after\' directory')
+            exit(1)
+
         csv_files = {}
-        path = os.getcwd()
-        for root, dirs, files in os.walk(path):
+
+        before_path = os.getcwd() + '\\before'
+        for root, dirs, files in os.walk(before_path):
             for file in files:
                 if file.endswith(".csv"):
-                    filename = file.split('-')[0]
-                    if filename in csv_files.keys():
-                        csv_files[filename] = csv_files[filename] + 1
+                    if file in csv_files.keys():
+                        csv_files[file] = csv_files[file] + 1
                     else:
-                        csv_files[filename] = 1
+                        csv_files[file] = 1
+
+        after_path = os.getcwd() + '\\after'
+        for root, dirs, files in os.walk(after_path):
+            for file in files:
+                if file.endswith('.csv'):
+                    if file in csv_files.keys():
+                        csv_files[file] = csv_files[file] + 1
+                    else:
+                        csv_files[file] = 1
+
         for i in csv_files:
             if csv_files[i] == 2:
-                result = compare_files(i + '-A.csv', i + '-B.csv')
+                result = compare_files(
+                    before_path + '\\' + i, after_path + '\\' + i)
                 results.update(result)
             elif csv_files[i] == 1:
-                results[filename] = "Missing second file"
+                results[i] = 'Missing second file to compare with'
+            else:
+                results[i] = 'Found more than 2 copies'
 
     print('\n\n')
     print('RESULTS'.center(50))
